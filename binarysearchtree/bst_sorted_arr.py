b077f6b10cedf473fbb820674eca141a71d82c48
danishshaikh06 <shaikhdanish.sd06@gmail.com>
2026-04-21 19:26:03 +0530

    making a bst using sorted array

---
+++ binarysearchtree/bst_sorted_arr.py
@@ -0,0 +1,28 @@
+# make a binary search tree given an sorted array- leetcode 108
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def helper(self, nums, st,end):
+        if st > end:
+            return None 
+
+        mid = (st + end) // 2
+        root = TreeNode(nums[mid])
+
+        root.left = self.helper(nums, st, mid - 1)
+        root.right = self.helper(nums, mid + 1, end)
+
+        return root
+
+
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: Optional[TreeNode]
+        """
+        return self.helper(nums, 0 , len(nums) - 1 )
