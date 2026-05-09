dc3458ed8f4c6adbe660f9cda956d373122fa432
danishshaikh06 <shaikhdanish.sd06@gmail.com>
2026-05-09 21:42:38 +0530

    Merge two sets using optimal code

---
+++ Graphs/union_find.py
@@ -0,0 +1,28 @@
+# disjoint set union or union find -> to merge two set 
+
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])   # path compression for optimization 
+        return self.parent[x]
+
+    def union(self, x, y):
+        px = self.find(x)
+        py = self.find(y)
+
+        if px == py:
+            return True   # cycle founnnd
+
+        if self.rank[px] < self.rank[py]:
+            self.parent[px] = py
+        elif self.rank[px] > self.rank[py]:
+            self.parent[py] = px
+        else:
+            self.parent[py] = px
+            self.rank[px] += 1
+
+        return False
