f796bd767fb1f959b6e90fe34c10eda7cca4a622
danishshaikh06 <shaikhdanish.sd06@gmail.com>
2026-06-21 23:45:34 +0530

    odd even linked list merging

---
+++ linkedlist/odd_even_ll.py
@@ -0,0 +1,31 @@
+#leetcode -> 328
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if head and head.next:
+            odd = head 
+            even = head.next 
+            evenhead = even
+
+            while even and even.next:
+
+                odd.next = odd.next.next
+                even.next = even.next.next 
+
+                odd = odd.next 
+                even = even.next 
+
+
+            odd.next = evenhead
+
+        return head
+
+    
